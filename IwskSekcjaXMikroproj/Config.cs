56114cad353311c80b693f2425db88851c50d1ec
Marcin Knyc <maroxesen@gmail.com>
2023-06-13 10:27:02 +0200

    Second commit

--- IwskSekcjaXMikroproj/Config.cs
+++ IwskSekcjaXMikroproj/Config.cs
@@ -14,12 +14,32 @@ namespace IwskSekcjaXMikroproj
         public int DataBitsNumber { get; set; } // 7 or 8
         public char ControlLetter { get; set; } //E, O, N
         public FlowControl FlowControlType { get; set; } // 0 - None, 
-        public TransmissionType TransmissionType { get; set; } // only textual is obligatory
+        public TransmissionType TransmissionType { get; set; } // only textual is obligator
         // buttons: Nadawanie, Odbiór and Ping
+        // powinno być okienko z tekstem wysłanej / odebranej ramki w dwóch formach: hexadecymalna i tekstowa
+
+        public Config() 
+        {
+            Validate();
+        }
+        public void Validate()
+        {
+            if (!(TransmissionSpeedBytes >= 150 && TransmissionSpeedBytes <= 115000))
+                throw new Exception("TransmissionSpeed must be between 150 and 115000");
+
+            //continue this validation
+        }
 
         #region master
+        public string MasterTransmittedTextualData { get; set; }
+        public int MasterTimeoutMiliseconds { get; set; } // 0-10s
+        public int MasterRetriesNumber { get; set; } // 0-5
+        public int MasterTimeLimit { get; set; } // 0-1s
+
         #endregion
         #region slave
+        public int SlaveAddress { get; set; }
+        public int SlaveTimeLimit { get; set; } // 0-1s
         #endregion
     }
 }
---
+++ IwskSekcjaXMikroproj/Transmit.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IwskSekcjaXMikroproj
+{
+    public class Transmit
+    {
+        public Transmit() { }
+        public void TransmitData(Config config) { }
+        public void RecieveData(Config config) { }
+        public void PingYourSlave(Config config) { }
+    }
+}
